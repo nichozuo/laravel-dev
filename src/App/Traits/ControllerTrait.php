0d95c57c6acd18e290f232403bd5e73d080f95e0
helloworld <hello@world.com>
2023-11-06 16:25:58 +0800

    add perPage

--- src/App/Traits/ControllerTrait.php
+++ src/App/Traits/ControllerTrait.php
@@ -4,6 +4,7 @@ namespace LaravelDev\App\Traits;
 
 
 use Exception;
+use LaravelDev\App\Exceptions\Err;
 
 trait ControllerTrait
 {
@@ -20,4 +21,24 @@ trait ControllerTrait
                 $params[$key] = bcrypt($params[$key]);
         }
     }
+
+    /**
+     * @intro 获得分页size
+     * @return int
+     * @throws Err
+     */
+    protected function perPage(): int
+    {
+        $params = request()->only('perPage');
+        if (!isset($params['perPage']) || !is_numeric($params['perPage']))
+            return 20;
+
+        $allow = config('project.perPageAllow', [10, 20, 50, 100,500,1000]);
+        if (!in_array($params['perPage'], $allow))
+            Err::Throw('分页数据不在规定范围内');
+
+        return (int)$params['perPage'];
+    }
+
+
 }
